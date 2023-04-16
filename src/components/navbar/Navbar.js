95075eb3144f2e1c7f99cbb0140c29c91e40850a
ramawachar <ramawachar621@gmail.com>
2023-04-17 02:47:27 +0530

    changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -44,12 +44,12 @@ const Navbar = () => {
           <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-gray-900 p-4 scrollbar-hide">
             <div className="flex flex-col gap-8 py-2 relative">
               <div>
-                <img className="w-32" src={logo} alt="logo" />
-                <p className="text-sm text-gray-400 mt-2">
+                {/* <img className="w-32" src={logo} alt="logo" /> */}
+                {/* <p className="text-sm text-gray-400 mt-2">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Earum soluta perspiciatis molestias enim cum repellat, magnam
                   exercitationem distinctio aliquid nam.
-                </p>
+                </p> */}
               </div>
               <ul className="flex flex-col gap-4">
                 {navLinksdata.map((item) => (
