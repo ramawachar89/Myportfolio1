34ee363ed2573e25152a94f790363e00ffe8112c
ramawachar <ramawachar621@gmail.com>
2023-04-17 23:34:29 +0530

    change

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 import {logo} from "../../assets/index"
-
+//footer
 const Footer = () => {
   return (
     <div className="w-full py-20 h-auto border-b-[1px] border-b-black grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-4 gap-8">
